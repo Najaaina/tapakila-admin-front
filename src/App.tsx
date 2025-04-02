f812a8118a4f0095a527d806be9025deec268dee
AroNicolas <hei.nicolas.5@gmail.com>
2025-04-02 15:52:55 +0300

    feat: add authProvider

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import UserList from './operations/user/UserList.tsx';
 
 export default function App(): JSX.Element {
     return (
-        <Admin>
+        <Admin authProvider={authProvider} loginPage={LoginWithEmail}>
             <Resource name={'users'} list={UserList} show={UserShow} />
             <Resource name={'events'} list={ListGuesser} show={ShowGuesser} edit={EditGuesser} />
             <Resource name={'ticket'} list={ListGuesser} show={ShowGuesser} edit={EditGuesser} />
