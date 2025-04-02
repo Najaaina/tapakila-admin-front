cb8deaca8e685ec06b836ad97739ab075f2ce882
Liantsoa-Najaina <najaina.andriamampianina@gmail.com>
2025-04-02 12:10:25 +0300

    chores: refactoring and addition of backend url in .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Admin, Resource } from "react-admin";
-import { JSX } from "react";
-import UserShow from "./operations/user/UserShow.tsx";
-import UserList from "./operations/user/UserList.tsx";
+import { Admin, EditGuesser, ListGuesser, Resource, ShowGuesser } from 'react-admin';
+import { JSX } from 'react';
+import UserShow from './operations/user/UserShow.tsx';
+import UserList from './operations/user/UserList.tsx';
 
 export default function App(): JSX.Element {
-  return (
-    <Admin>
-      <Resource name={"users"} list={UserList} show={UserShow} />
-    </Admin>
-  );
+    return (
+        <Admin>
+            <Resource name={'users'} list={UserList} show={UserShow} />
+            <Resource name={'events'} list={ListGuesser} show={ShowGuesser} edit={EditGuesser} />
+            <Resource name={'ticket'} list={ListGuesser} show={ShowGuesser} edit={EditGuesser} />
+        </Admin>
+    );
 }
